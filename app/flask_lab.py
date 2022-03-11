8fcce5452f845efb0480b0a67364f8beabb1b622
Lloyd Hamilton <lloydhamilton1991@gmail.com>
2022-03-11 12:34:40 +0000

    inital commit

---
+++ app/flask_lab.py
@@ -0,0 +1,31 @@
+
+from flask import Flask
+from flask import Response
+from flask import request
+import joblib
+import pandas as pd
+import json
+import numpy as np
+
+# model = None
+app = Flask(__name__)
+
+@app.route('/')
+def home_endpoint():
+    return 'Welcome to CodeClan'
+
+@app.route('/predict', methods=['POST'])
+def predict_endpoint():
+    data = request.json
+    features = np.array(json.loads(data['features']))
+    preds = (
+        model
+        .predict(features)
+        .tolist()
+    )
+    response = json.dumps(preds)
+    return Response(response, status=200, mimetype="application/json")
+
+if __name__ == '__main__':
+    model = joblib.load('log_model.joblib')
+    app.run(host='0.0.0.0', port=80)
